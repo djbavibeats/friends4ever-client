e54486256a9791a2d19dba8b68f555148eb6d57e
djbavibeats <justin@voltcreative.com>
2023-10-03 15:37:11 -0400

    udpate url

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -22,9 +22,9 @@ export default function Intro({ screen, handleScreenChange, user, handlePopulate
     const [ loggedIn, setLoggedIn ] = useState(false)
 
     // Production
-    // const url = 'https://friends4ever-server.onrender.com'
+    const url = 'https://friends4ever-server.onrender.com'
     // Development
-    const url = 'http://localhost:5000'
+    // const url = 'http://localhost:5000'
 
     useEffect(() => {
         const spotifyToken = getTokenFromUrl()
